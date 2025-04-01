5276ea5800a11df781ec4cfd10bf6e72d6c78e25
HoLLy <dario.db@hotmail.com>
2025-04-01 17:50:22 +0200

    Add Zed configuration for Biome

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         default = pkgs.mkShell {
           # The Nix packages provided in the environment
           # Add any you need here
-          packages = with pkgs; [ nodejs_22 ];
+          packages = with pkgs; [ nodejs_22 biome ];
 
           # Set any environment variables for your dev shell
           env = { };
